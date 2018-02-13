66a074ec9058b6ff6a9b2f26ac81d0f824caa860
unknown <dipisha03@gmail.com>
2018-02-13 18:13:02 -0500

    second commit

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO burgers (burger_name) VALUES ('Cheese');
+INSERT INTO burgers (burger_name) VALUES ('Veggie');
+INSERT INTO burgers (burger_name) VALUES ('Chicken');
+
+SELECT * FROM burgers 
