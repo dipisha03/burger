5c906e7de82b507616348491df2348e10e484d26
unknown <dipisha03@gmail.com>
2018-02-15 17:10:14 -0500

    new commit

---
+++ config/orm.js
@@ -0,0 +1,12 @@
+// Import MySQL connection.
+var connection = require("../config/connection.js");
+
+// Methods that will execute MySQL commands in the controllers 
+
+selectAll()
+insertOne()
+updateOne()
+
+
+// Export the orm object for the model (burger.js).
+module.exports = orm;
